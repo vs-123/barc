b1eaad9f77ca5e71c1f00f59839ce198bc5850ad
VS-123 <>
2026-02-12 05:14:03 +0000

    barc: add multi-file support
    - add ability to compress multiple files into one archive
    - add vice versa for decompression
    - remove unnecessary type casts (turning on -Weverything was a bad idea LOL)
    - use stdint types cuz it looks better

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void
-rlewrite (unsigned char c, int count, FILE *out)
+rlewrite (uint8_t c, int32_t count, FILE *out)
 {
    while (count > 0)
       {
@@ -17,7 +18,6 @@ rlewrite (unsigned char c, int count, FILE *out)
                fputc (c, out);
                fputc (c, out);
                count -= 2;
-
                if (count > 255)
                   {
                      fputc (255, out);
@@ -33,21 +33,20 @@ rlewrite (unsigned char c, int count, FILE *out)
 }
 
 static const char *bwtptr;
-static int bwtlen;
+static int32_t bwtlen;
 
 int
 bwtcmp (const void *a, const void *b)
 {
-   int idx1, idx2, i;
-   unsigned char c1, c2;
+   int32_t idx1, idx2, i;
 
-   idx1 = *(const int *)a;
-   idx2 = *(const int *)b;
+   idx1 = *(const int32_t *)a;
+   idx2 = *(const int32_t *)b;
 
    for (i = 0; i < bwtlen; i++)
       {
-         c1 = bwtptr[(idx1 + i) % bwtlen];
-         c2 = bwtptr[(idx2 + i) % bwtlen];
+         uint8_t c1 = bwtptr[(idx1 + i) % bwtlen];
+         uint8_t c2 = bwtptr[(idx2 + i) % bwtlen];
          if (c1 != c2)
             {
                return c1 - c2;
@@ -56,23 +55,30 @@ bwtcmp (const void *a, const void *b)
    return 0;
 }
 
-void
+long
 fbwtenc (FILE *in, FILE *out)
 {
-   int n, primidx, rlecnt, i, rank;
+   long startpos, endpos;
+   int32_t n, primidx, rlecnt, i, rank;
    char *s;
-   int *idxs;
-   unsigned char dict[256];
-   unsigned char lastc, c, mtfval;
+   int32_t *idxs;
+   uint8_t dict[256];
+   uint8_t lastc, c, mtfval;
 
+   startpos = ftell (out);
    fseek (in, 0, SEEK_END);
-   n = (int)ftell (in);
+   n = ftell (in);
    rewind (in);
+
+   if (n == 0)
+      {
+         return 0;
+      }
 
    s = malloc (n);
    fread (s, 1, n, in);
 
-   idxs = malloc (n * sizeof (int));
+   idxs = malloc (n * sizeof (int32_t));
    for (i = 0; i < n; i++)
       {
          idxs[i] = i;
@@ -80,7 +86,7 @@ fbwtenc (FILE *in, FILE *out)
 
    bwtptr = s;
    bwtlen = n;
-   qsort (idxs, n, sizeof (int), bwtcmp);
+   qsort (idxs, n, sizeof (int32_t), bwtcmp);
 
    primidx = 0;
    for (i = 0; i < n; i++)
@@ -92,11 +98,11 @@ fbwtenc (FILE *in, FILE *out)
             }
       }
 
-   fwrite (&primidx, sizeof (int), 1, out);
+   fwrite (&primidx, sizeof (int32_t), 1, out);
 
    for (i = 0; i < 256; i++)
       {
-         dict[i] = (unsigned char)i;
+         dict[i] = i;
       }
 
    rlecnt = 0;
@@ -117,8 +123,7 @@ fbwtenc (FILE *in, FILE *out)
                dict[0] = c;
             }
 
-         mtfval = (unsigned char)rank;
-
+         mtfval = rank;
          if (i == 0)
             {
                lastc  = mtfval;
@@ -135,29 +140,30 @@ fbwtenc (FILE *in, FILE *out)
                rlecnt = 1;
             }
       }
-
    rlewrite (lastc, rlecnt, out);
 
    free (s);
    free (idxs);
+   return ftell (out) - startpos;
 }
 
 void
-fbwtdec (FILE *in, FILE *out)
+fbwtdec (FILE *in, FILE *out, int32_t datalen)
 {
-   int primidx, c, rank, sum, crnt, extra, lastc, haslast;
-   size_t cap, n, i, k;
-   unsigned char *r;
-   unsigned char dict[256];
-   int arrcnt[256] = { 0 };
-   int first[256];
-   int curcnt[256] = { 0 };
-   int *T;
+   int32_t primidx, lastc, haslast, c, extra, k, rank, sum, crnt;
+   long nread;
+   size_t cap, n, i;
+   uint8_t *r, dict[256], curc;
+   int32_t arrcnt[256] = { 0 }, first[256], curcnt[256] = { 0 };
+   int32_t *T;
 
-   if (fread (&primidx, sizeof (int), 1, in) != 1)
+   if (datalen <= 0)
       {
          return;
       }
+
+   fread (&primidx, sizeof (int32_t), 1, in);
+   nread = sizeof (int32_t);
 
    cap     = 1024;
    n       = 0;
@@ -165,26 +171,29 @@ fbwtdec (FILE *in, FILE *out)
    lastc   = -1;
    haslast = 0;
 
-   while ((c = fgetc (in)) != EOF)
+   while (nread < datalen)
       {
+         c = fgetc (in);
+         nread++;
          if (n >= cap)
             {
                cap *= 2;
                r = realloc (r, cap);
             }
-         r[n++] = (unsigned char)c;
+         r[n++] = c;
 
          if (haslast && c == lastc)
             {
                extra = fgetc (in);
-               for (k = 0; k < (size_t)extra; k++)
+               nread++;
+               for (k = 0; k < extra; k++)
                   {
                      if (n >= cap)
                         {
                            cap *= 2;
                            r = realloc (r, cap);
                         }
-                     r[n++] = (unsigned char)c;
+                     r[n++] = c;
                   }
                haslast = 0;
                lastc   = -1;
@@ -198,17 +207,18 @@ fbwtdec (FILE *in, FILE *out)
 
    for (i = 0; i < 256; i++)
       {
-         dict[i] = (unsigned char)i;
+         dict[i] = i;
       }
+
    for (i = 0; i < n; i++)
       {
          rank = r[i];
-         c    = dict[rank];
-         r[i] = (unsigned char)c;
+         curc = dict[rank];
+         r[i] = curc;
          if (rank > 0)
             {
                memmove (&dict[1], &dict[0], rank);
-               dict[0] = (unsigned char)c;
+               dict[0] = curc;
             }
       }
 
@@ -216,13 +226,15 @@ fbwtdec (FILE *in, FILE *out)
       {
          arrcnt[r[i]]++;
       }
+
    sum = 0;
    for (i = 0; i < 256; i++)
       {
          first[i] = sum;
          sum += arrcnt[i];
       }
-   T = malloc (n * sizeof (int));
+
+   T = malloc (n * sizeof (int32_t));
    for (i = 0; i < n; i++)
       {
          T[first[r[i]] + curcnt[r[i]]] = i;
@@ -240,25 +252,109 @@ fbwtdec (FILE *in, FILE *out)
    free (r);
 }
 
-int
-main (void)
+void
+arcfile (const char *fname, FILE *outarc)
 {
-   FILE *fin, *fenc, *fbwt, *fdec;
+   FILE *in;
+   int32_t namelen, placeholder, cmpsz;
+   long sizepos, endpos;
 
-   fin  = fopen ("test.txt", "rb");
-   fenc = fopen ("test.barc", "wb");
+   in = fopen (fname, "rb");
+   if (!in)
+      {
+         fprintf (stderr, "[BARC ERROR] %s NOT OPEN\n", fname);
+         return;
+      }
 
-   fbwtenc (fin, fenc);
-   fclose (fin);
-   fclose (fenc);
+   namelen = strlen (fname);
+   fwrite (&namelen, sizeof (int32_t), 1, outarc);
+   fwrite (fname, 1, namelen, outarc);
 
-   fbwt = fopen ("test.barc", "rb");
-   fdec = fopen ("decoded.txt", "wb");
+   sizepos     = ftell (outarc);
+   placeholder = 0;
+   fwrite (&placeholder, sizeof (int32_t), 1, outarc);
 
-   fbwtdec (fbwt, fdec);
-   fclose (fbwt);
-   fclose (fdec);
+   printf ("[BARC COMPRESS] %s\n", fname);
+   cmpsz = fbwtenc (in, outarc);
 
-   printf ("SUCCESS\n");
+   endpos = ftell (outarc);
+   fseek (outarc, sizepos, SEEK_SET);
+   fwrite (&cmpsz, sizeof (int32_t), 1, outarc);
+   fseek (outarc, endpos, SEEK_SET);
+
+   fclose (in);
+}
+
+void
+exarc (FILE *inarc)
+{
+   int32_t namelen, cmpsz;
+   char *fname;
+   char outname[256];
+   FILE *out;
+
+   while (fread (&namelen, sizeof (int32_t), 1, inarc) == 1)
+      {
+         if (namelen == -1)
+            {
+               break;
+            }
+
+         fname = malloc (namelen + 1);
+         fread (fname, 1, namelen, inarc);
+         fname[namelen] = '\0';
+
+         fread (&cmpsz, sizeof (int32_t), 1, inarc);
+         snprintf (outname, sizeof (outname), "BARC_EXT_%s", fname);
+
+         out = fopen (outname, "wb");
+         if (out)
+            {
+               printf ("[BARC EXTRACT] %s -> %s\n", fname, outname);
+               fbwtdec (inarc, out, cmpsz);
+               fclose (out);
+            }
+         free (fname);
+      }
+}
+
+int
+main (int argc, char *argv[])
+{
+   char mode;
+   FILE *arc;
+   int32_t i, eofmark;
+
+   if (argc < 3)
+      {
+         printf ("[BARC COMPRESS]   BARC C ARCHIVE.BARC FILE1.TXT FILE2.TXT FILE3.TXT...\n");
+         printf ("[BARC DECOMPRESS] BARC X ARCHIVE.BARC\n");
+         return 1;
+      }
+
+   mode = argv[1][0];
+   if (mode == 'c')
+      {
+         arc = fopen (argv[2], "wb");
+         for (i = 3; i < argc; i++)
+            {
+               arcfile (argv[i], arc);
+            }
+
+         eofmark = -1;
+         fwrite (&eofmark, sizeof (int32_t), 1, arc);
+         fclose (arc);
+         printf ("[BARC] ARCHIVED\n");
+      }
+   else if (mode == 'x')
+      {
+         arc = fopen (argv[2], "rb");
+         if (arc)
+            {
+               exarc (arc);
+               fclose (arc);
+            }
+         printf ("[BARC] EXTRACTED\n");
+      }
    return 0;
 }
